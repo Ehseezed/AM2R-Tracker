7f621d791e67b3400798b1e1e6670ecae503199d
Ehseezed <aeneas.zaremba@gmail.com>
2024-03-17 15:39:41 -0500

    Auto tracking through AP complete

--- scripts/autotracking/item_mapping.lua
+++ scripts/autotracking/item_mapping.lua
@@ -2,8 +2,24 @@
 -- first value is the code of the target item and the second is the item type (currently only "toggle", "progressive" and "consumable" but feel free to expand for your needs!)
 -- here are the SM items as an example: https://github.com/Cyb3RGER/sm_ap_tracker/blob/main/scripts/autotracking/item_mapping.lua
 ITEM_MAPPING = {
-    [00000] = {"toggle", "toggle"},
-    [00001] = {"progressive", "progressive"},
-    [00002] = {"consumable", "consumable"},
-    [00003] = {"progressive_toggle", "progressive"} -- progressive_toggle should work with the progressive type but might need additional code to work for your needs
+    [8678016] = {"Charge Beam", "toggle"},
+    [8678017] = {"Wave Beam", "toggle"},
+    [8678020] = {"Ice Beam", "toggle"},
+    [8678018] = {"Spazer Beam", "toggle"},
+    [8678019] = {"Plasma Beam", "toggle"},
+    [8678014] = {"Varia Suit", "toggle"},
+    [8678015] = {"Gravity Suit", "toggle"},
+    [8678005] = {"Morph Ball", "toggle"},
+    [8678006] = {"Power Grip", "toggle"},
+    [8678008] = {"Spider Ball", "toggle"},
+    [8678010] = {"Spring Ball", "toggle"},
+    [8678007] = {"Bombs", "toggle"},
+    [8678009] = {"Hi Jump Boots", "toggle"},
+    [8678011] = {"Space Jump", "toggle"},
+    [8678012] = {"Speed Booster", "toggle"},
+    [8678013] = {"Screw Attack", "toggle"},
+    [8678003] = {"Energy Tank", "consumable"},
+    [8678000] = {"Missiles", "consumable"},
+    [8678001] = {"Super Missiles", "consumable"},
+    [8678002] = {"Power Bombs", "consumable"}
 }
